1d3f5aeb843b480e6d744481b0817e5d33d28f8e
Hamid Ali Khan <157398894+corps3d@users.noreply.github.com>
2024-11-02 17:57:54 +0500

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,5 +1,11 @@
+# api/webhook.py
+
 def handler(event, context):
+    # Basic response format expected by Vercel
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/plain"
+        },
         "body": "Hello, world!"
     }
